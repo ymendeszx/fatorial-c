8c8ceb675e947be35753e85989f2906698376244
mendes <ymendeszx@gmail.com>
2026-05-28 18:39:53 -0300

    Delete function_fatorial.c

--- function_fatorial.c
+++
@@ -1,9 +0,0 @@
-int fatorial(int numero)
-{
-    int resultado = 1;
-    for(int contador=numero;contador>=1;contador--)
-    {
-        resultado = resultado * contador;
-    }
-    return resultado
-}
